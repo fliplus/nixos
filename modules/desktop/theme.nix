363cd10c22ce3ea7e23f4d33a048a7a20c2a962b
Filipe Abreu <134308239+fliplus@users.noreply.github.com>
2026-01-12 16:15:00 +0000

    Add cursor, icons and application theme

---
+++ modules/desktop/theme.nix
@@ -0,0 +1,32 @@
+{
+  flake.nixosModules.core = { pkgs, user, ... }:
+
+  {
+    environment.systemPackages = with pkgs; [
+      kdePackages.breeze
+      adw-gtk3
+      papirus-icon-theme
+    ];
+
+    environment.sessionVariables = {
+      XCURSOR_THEME = "Breeze_Light";
+    };
+
+    programs.dconf = {
+      enable = true;
+
+      profiles.user.databases = [
+        {
+          settings = {
+            "org/gnome/desktop/interface" = {
+              color-scheme = "prefer-dark";
+              cursor-theme = "Breeze_Light";
+              gtk-theme = "adw-gtk3-dark";
+              icon-theme = "Papirus-Dark";
+            };
+          };
+        }
+      ];
+    };
+  };
+}
