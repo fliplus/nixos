2186c17d2458c66f583b660b67c491b72e609f01
Filipe Abreu <134308239+fliplus@users.noreply.github.com>
2026-02-03 12:33:26 +0000

    Add Cemu

--- modules/apps/games.nix
+++ modules/apps/games.nix
@@ -11,11 +11,17 @@
 
     environment.systemPackages = with pkgs; [
       heroic
+      cemu
     ];
 
     preferences.persist.home.directories = [
       ".local/share/Steam"
+
       ".config/heroic"
+
+      ".config/Cemu"
+      ".local/share/Cemu"
+
       "Games"
     ];
   };
