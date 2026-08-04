abf7b8464a0e0d308220f4eea784b910c130fbdf
Filipe Abreu <134308239+fliplus@users.noreply.github.com>
2026-08-04 20:18:42 +0100

    Symlink JDKs to ~/.jdks for Gradle autodetection

--- modules/apps/jetbrains.nix
+++ modules/apps/jetbrains.nix
@@ -1,13 +1,32 @@
+{ lib, ... }:
 {
   flake.nixosModules.core =
-    { pkgs, ... }:
+    {
+      config,
+      pkgs,
+      ...
+    }:
+    let
+      inherit (config.preferences.system) user;
+
+      jdks = with pkgs.javaPackages.compiler.temurin-bin; {
+        "25" = jdk-25;
+        "21" = jdk-21;
+        "17" = jdk-17;
+        "8" = jdk-8;
+      };
+    in
     {
       environment.systemPackages = with pkgs; [
         jetbrains.idea
         jetbrains.ruby-mine
 
-        javaPackages.compiler.temurin-bin.jdk-25
+        jdks."25"
       ];
+
+      hjem.users.${user}.files = lib.mapAttrs' (
+        version: jdk: lib.nameValuePair ".jdks/temurin-${version}" { source = jdk; }
+      ) jdks;
 
       preferences.persist.home.directories = [
         ".config/JetBrains"
