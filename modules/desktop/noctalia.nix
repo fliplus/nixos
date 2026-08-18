96b63141a1f523eae3eb0699c07a1b8ab6e1a44e
Filipe Abreu <134308239+fliplus@users.noreply.github.com>
2026-08-18 11:09:52 +0100

    Add Noctalia settings

--- modules/desktop/noctalia.nix
+++ modules/desktop/noctalia.nix
@@ -21,6 +21,8 @@
         inputs.noctalia.nixosModules.default
       ];
 
+      hjem.extraModules = [ inputs.noctalia.hjemModules.default ];
+
       programs.noctalia = {
         enable = true;
         systemd.enable = true;
@@ -31,6 +33,105 @@
       ];
 
       hjem.users.${user} = {
+        programs.noctalia = {
+          enable = true;
+
+          settings = {
+            backdrop.enabled = true;
+
+            bar.default = {
+              background_opacity = 0.85;
+              end = [
+                "tray"
+                "nix-monitor"
+                "clipboard"
+                "recorder"
+                "bluetooth"
+                "volume"
+                "control-center"
+              ];
+              margin_ends = 0;
+              padding = 6;
+              radius = 0;
+              start = [
+                "workspaces"
+                "sysmon"
+                "ram"
+                "media"
+              ];
+              thickness = 24;
+            };
+
+            control_center.calendar.show_events_card = false;
+
+            location.auto_locate = true;
+
+            nightlight.enabled = true;
+
+            notification = {
+              monitors = [ (lib.last config.preferences.monitors).name ];
+              position = if config.preferences.system.isLaptop then "top_right" else "top_left";
+            };
+
+            osd.kinds.media = false;
+
+            plugin_settings."noctalia/screen_recorder" = {
+              audio_source = "both";
+              copy_to_clipboard = true;
+              replay_duration = 60;
+              replay_enabled = true;
+              restore_portal = true;
+            };
+
+            plugins.enabled = [
+              "noctalia/wallhaven"
+              "noctalia/screen_recorder"
+              "cleboost/jetbrains-provider"
+              "radimous/prismlauncher-instances"
+              "avivbintangaringga/nix-monitor"
+            ];
+
+            shell = {
+              polkit_agent = true;
+              screen_time_enabled = true;
+
+              animation.speed = 1.5;
+
+              panel.transparency_mode = "soft";
+            };
+
+            theme.templates.builtin_ids = [
+              "ghostty"
+              "niri"
+            ];
+
+            widget = {
+              media.hide_when_no_media = true;
+
+              "nix-monitor" = {
+                colorize_glyph = false;
+                show_text = false;
+                type = "avivbintangaringga/nix-monitor:nix-monitor";
+              };
+
+              recorder.type = "noctalia/screen_recorder:recorder";
+
+              sysmon.show_value = false;
+
+              tray.drawer = true;
+
+              volume.show_label = false;
+
+              workspaces = {
+                focused_output_only = true;
+                hide_when_empty = true;
+                label_source = "name";
+                style = "minimal";
+              };
+            };
+          };
+        };
+
         rum.desktops.niri = {
           binds = {
             "Mod+Space".spawn = [
