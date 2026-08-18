5dd69df647741f1bad6efb0c2b636ced557d47ef
Filipe Abreu <134308239+fliplus@users.noreply.github.com>
2026-08-18 10:11:07 +0100

    Add OBS

---
+++ modules/apps/obs.nix
@@ -0,0 +1,17 @@
+{
+  flake.nixosModules.core =
+    { pkgs, ... }:
+    {
+      environment.systemPackages = with pkgs; [
+        (pkgs.wrapOBS {
+          plugins = with pkgs.obs-studio-plugins; [
+            obs-pipewire-audio-capture
+          ];
+        })
+      ];
+
+      preferences.persist.home.directories = [
+        ".config/obs-studio"
+      ];
+    };
+}
